c27b5f02c4d33872e6042cd45279ebb252503e4e
William Boehringer <williamboehringer1@gmail.com>
2022-11-03 18:02:51 -0300

    updated static dir

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / 'upload/'
 MEDIA_URL = '/media/'
--- jobapp/urls.py
+++ jobapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', include('app.urls')),
     path('', include('accounts.urls')),
     path('uploads/', include('uploadapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #\
+#   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   
