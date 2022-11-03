6e7430ec11f466b7c74fe229fd503e6e8dae2fc7
William Boehringer <williamboehringer1@gmail.com>
2022-11-03 17:13:06 -0300

    updated static root

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('uploads/', include('uploadapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, documento_root=settings.STATIC_ROOT)
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   
